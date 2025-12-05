d0cbdc3ae7e7bba624b27580b8427ffbaa62477b
Arias08426 <jarias1028@cue.edu.co>
2025-12-05 18:41:41 -0500

    Actualizacion del pipeline y de la app.py en el backend

--- backend/app.py
+++ backend/app.py
@@ -110,4 +110,4 @@ def reset_data():
     return jsonify({"mensaje": "Datos reseteados"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
